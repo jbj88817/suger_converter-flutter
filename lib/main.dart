f3f4958511ec3b443f60b37aece39044794859d5
Bojie Jiang <jiangbojie@gmail.com>
2023-01-19 23:38:08 -0800

    Add A1C

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,9 @@ class MyApp extends StatelessWidget {
 class MainPage extends StatelessWidget {
   MainPage({Key? key}) : super(key: key);
 
-  var txtM = TextEditingController();
-  var txtA = TextEditingController();
+  var txtM = TextEditingController(); // mmol/L
+  var txtA = TextEditingController(); // mg/dL
+  var txtB = TextEditingController(); // A1C
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +46,7 @@ class MainPage extends StatelessWidget {
                     onChanged: (value) {
                       var aInt = double.parse(value);
                       txtM.text = (aInt * 0.0555).toString();
+                      txtB.text = ((aInt + 46.7) / 28.7).toString();
                     },
                     controller: txtA,
                     keyboardType: const TextInputType.numberWithOptions(
@@ -83,6 +85,7 @@ class MainPage extends StatelessWidget {
                     onChanged: (value) {
                       var mmInt = double.parse(value);
                       txtA.text = (mmInt / 0.0555).toString();
+                      txtB.text = (((mmInt / 0.0555) + 46.7) / 28.7).toString();
                     },
                     controller: txtM,
                     keyboardType: const TextInputType.numberWithOptions(
@@ -107,6 +110,43 @@ class MainPage extends StatelessWidget {
                   width: 16,
                 ),
                 const Text(" mmol/L"),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {
+                      var a1cInt = double.parse(value);
+                      txtA.text = (28.7 * a1cInt - 46.7).toString();
+                      txtM.text = ((28.7 * a1cInt - 46.7) * 0.0555).toString();
+                    },
+                    controller: txtB,
+                    keyboardType: const TextInputType.numberWithOptions(
+                        decimal: true, signed: false),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp(r"[0-9.]")),
+                      TextInputFormatter.withFunction((oldValue, newValue) {
+                        try {
+                          final text = newValue.text;
+                          if (text.isNotEmpty) double.parse(text);
+                          return newValue;
+                        } catch (e) {}
+                        return oldValue;
+                      }),
+                    ],
+                    onTap: () {
+                      txtB.clear();
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                const Text(" A1C"),
               ],
             ),
           )
